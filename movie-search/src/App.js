29ff2b45bbcb0e9e012a234877b69ef51d0a4949
fbudiman <fbudiman14@hotmail.com>
2019-03-23 15:58:31 -0700

    sets up Movie component

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+// React
+import React, { Component } from 'react'
+// Styles
+import './App.css'
+// Services
 import { search } from './services/search'
+// Components
+import Movie from './components/Movie/Movie'
 
 class App extends Component {
 
@@ -8,6 +13,8 @@ class App extends Component {
         return (
             <div className="App">
                 hello world
+
+                <Movie />
             </div>
         )
     }
---
+++ movie-search/src/components/Movie/Movie.js
@@ -0,0 +1,12 @@
+// React
+import React from 'react'
+// Styles
+import './Movie.css'
+
+const Movie = movie => {
+	return (
+		<div>THIS BE A MOVIE</div>
+	)
+}
+
+export default Movie
